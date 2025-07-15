51daacd1b72af59edb60c74bd77e251c54df7769
littleRock0 <irochelle@byupathway.edu>
2025-07-15 17:52:37 -0600

    final draft

--- scripts/temples.js
+++ scripts/temples.js
@@ -18,3 +18,11 @@ if (authorMeta) {
 
 lastModifiedElement.innerHTML = `Last updated: ` +
     `${lastModifiedDate.toLocaleString()}`;
+
+const mainNav = document.querySelector('.navigation')
+const hamButton = document.querySelector('#menu');
+
+hamButton.addEventListener('click', () => {
+	mainNav.classList.toggle('show');
+	hamButton.classList.toggle('show');
+});
