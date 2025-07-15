272f27a07ae06c0d0009e183fb128d71dc4d08c4
littleRock0 <irochelle@byupathway.edu>
2025-07-14 23:24:29 -0600

    added JavaScript

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,8 +1,19 @@
 /*******************************************************************************
  * 
 *******************************************************************************/
-const ;
+const year = document.querySelector("#year");
+const author = document.querySelector("#author");
+const lastModifiedElement =document.querySelector("#lastModified")
+const today = new Date();
 
+let lastModifiedDate = new Date(document.lastModified);
 
+year.innerHTML = ` <span class="highlight"> ${today.getFullYear()}</span>`;
 
-alert(document.lastModified);
+const authorMeta = document.querySelector('meta[name="author"]');
+
+if (authorMeta) {
+  author.innerHTML = authorMeta.content;
+}
+
+lastModifiedElement.innerHTML = `Last updated: ${lastModifiedDate.toLocaleString()}`;
