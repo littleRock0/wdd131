26b51ef182bb6b02bbef84c4ca08fb6881e83ece
littleRock0 <irochelle@byupathway.edu>
2025-07-19 23:23:29 -0600

    started adding structure for dinamically loaded stuffs

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,9 +1,9 @@
-/*******************************************************************************
+/***************************************************************************
  * 
-*******************************************************************************/
+ **************************************************************************/
 const year = document.querySelector("#year");
 const author = document.querySelector("#author");
-const lastModifiedElement =document.querySelector("#lastModified")
+const lastModifiedElement = document.querySelector("#lastModified");
 const today = new Date();
 
 let lastModifiedDate = new Date(document.lastModified);
@@ -13,8 +13,8 @@ year.innerHTML = ` <span class="highlight"> ${today.getFullYear()}</span>`;
 const authorMeta = document.querySelector('meta[name="author"]');
 
 if (authorMeta) {
-  author.innerHTML = authorMeta.content;
+    author.innerHTML = authorMeta.content;
 }
 
 lastModifiedElement.innerHTML = `Last updated: ` +
-  `${lastModifiedDate.toLocaleString()}`;
+    `${lastModifiedDate.toLocaleString()}`;
--- scripts/place.js
+++ scripts/place.js
@@ -1,9 +1,13 @@
-/*******************************************************************************
+/***************************************************************************
  * 
-*******************************************************************************/
+ **************************************************************************/
 const year = document.querySelector("#year");
 const author = document.querySelector("#author");
-const lastModifiedElement =document.querySelector("#lastModified")
+const lastModifiedElement = document.querySelector("#lastModified");
+const temp = document.querySelector("#tempurature");
+const conditions = document.querySelector("#conditions");
+const wind = document.querySelector("#wind");
+const chill = document.querySelector("#chill");
 const today = new Date();
 
 let lastModifiedDate = new Date(document.lastModified);
@@ -13,8 +17,15 @@ year.innerHTML = ` <span class="highlight"> ${today.getFullYear()}</span>`;
 const authorMeta = document.querySelector('meta[name="author"]');
 
 if (authorMeta) {
-  author.innerHTML = authorMeta.content;
+    author.innerHTML = authorMeta.content;
 }
 
 lastModifiedElement.innerHTML = `Last updated: ` +
-  `${lastModifiedDate.toLocaleString()}`;
+    `${lastModifiedDate.toLocaleString()}`;
+
+/*********
+ * `Tempurature: ${temp}`
+ * `Conditions: ${condition}`
+ * `Wind: ${wind}`
+ * `Wind Chill: ${chill}`
+ */
