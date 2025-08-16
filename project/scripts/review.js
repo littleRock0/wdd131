a4f702addf47649753cf49e3c8d2d8498d5ab0ee
littleRock0 <irochelle@byupathway.edu>
2025-08-16 13:33:59 -0600

    now done except for dynamic generation

---
+++ project/scripts/review.js
@@ -0,0 +1,41 @@
+/***************************************************************************
+ * 
+ **************************************************************************/
+const year = document.querySelector("#year");
+const author = document.querySelector("#author");
+const lastModifiedElement = document.querySelector("#lastModified");
+const today = new Date();
+
+let lastModifiedDate = new Date(document.lastModified);
+
+year.innerHTML = ` <span class="highlight"> ${today.getFullYear()}</span>`;
+
+const authorMeta = document.querySelector('meta[name="author"]');
+
+if (authorMeta) {
+    author.innerHTML = authorMeta.content;
+}
+
+lastModifiedElement.innerHTML = `Last updated: ` +
+    `${lastModifiedDate.toLocaleString()}`;
+
+const mainNav = document.querySelector('.navigation')
+const hamButton = document.querySelector('#menu');
+
+hamButton.addEventListener('click', () => {
+    mainNav.classList.toggle('show');
+    hamButton.classList.toggle('show');
+});
+
+let reviewCount = Number(localStorage.getItem("reviewCount")) || 0;
+
+reviewCount++;
+
+localStorage.setItem("reviewCount", reviewCount);
+
+const main = document.querySelector("main");
+if (main) {
+    const msg = document.createElement("p");
+    msg.textContent = `You have submitted ${reviewCount} review(s).`;
+    main.appendChild(msg);
+};
