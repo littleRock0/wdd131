acc1f0137ca76a575f78e29bdf06fdeaad67f7e9
littleRock0 <irochelle@byupathway.edu>
2025-08-16 13:09:42 -0600

    filled in routes array and a few other small tweaks

--- project/scripts/remember-the-maine-script.js
+++ project/scripts/remember-the-maine-script.js
@@ -29,129 +29,136 @@ hamButton.addEventListener('click', () => {
 
 const routes = [
     {
-        id: "",
-        name: "",
-        averageRating: 0,
+        name: "About Face",
+        averageRating: 3,
+        difficulty: 5.8,
+        type: "Sport",
+    },
+    {
+        name: "Apple Juice",
+        averageRating: 3,
+        difficulty: 5.9,
+        type: "Sport",
+    },
+    {
+        name: "Apple Juice with Pulp",
+        averageRating: 3,
+        difficulty: "5.10a",
+        type: "Sport",
+    },
+    {
+        name: "Capitan Sigsbee",
+        averageRating: 3,
+        difficulty: 5.9,
+        type: "Sport",
+    },
+    {
+        name: "Do the French like Cheese?",
+        averageRating: 2,
+        difficulty: "5.10d",
+        type: "Sport",
+    },
+    {
+        name: "Independence Arch",
+        averageRating: 4,
+        difficulty: "5.10a",
+        type: "Sport",
+    },
+    {
+        name: "J.R.O.T.C.",
+        averageRating: 3,
         difficulty: 5.0,
         type: "",
     },
     {
-        id: "",
-        name: "",
+        name: "Lichen to Me",
         averageRating: 0,
-        difficulty: 5.0,
+        difficulty: "5.11",
         type: "",
     },
     {
-        id: "",
-        name: "",
-        averageRating: 0,
-        difficulty: 5.0,
-        type: "",
+        name: "Lieutenant Friend",
+        averageRating: 2,
+        difficulty: 5.8,
+        type: "Sport",
     },
     {
-        id: "",
-        name: "",
-        averageRating: 0,
-        difficulty: 5.0,
-        type: "",
+        name: "Lizzard Fingers",
+        averageRating: 4,
+        difficulty: "5.11C",
+        type: "Sport",
     },
     {
-        id: "",
-        name: "",
-        averageRating: 0,
-        difficulty: 5.0,
-        type: "",
+        name: "Maine Squeeze",
+        averageRating: 2,
+        difficulty: 5.11,
+        type: "TR",
     },
     {
-        id: "",
-        name: "",
-        averageRating: 0,
-        difficulty: 5.0,
-        type: "",
+        name: "Petrogrift",
+        averageRating: 3,
+        difficulty: "5.11a",
+        type: "Sport",
     },
     {
-        id: "",
-        name: "",
-        averageRating: 0,
-        difficulty: 5.0,
-        type: "",
+        name: "Purely Accidental",
+        averageRating: 2.5,
+        difficulty: "5.11a/b",
+        type: "Sport",
     },
     {
-        id: "",
-        name: "",
-        averageRating: 0,
-        difficulty: 5.0,
-        type: "",
+        name: "Teeter-Totter/Sandstorm",
+        averageRating: 3,
+        difficulty: 5.9,
+        type: "Sport",
     },
     {
-        id: "",
-        name: "",
-        averageRating: 0,
-        difficulty: 5.0,
-        type: "",
+        name: "Thunderstorm",
+        averageRating: 3,
+        difficulty: 5.9,
+        type: "Trad",
     },
     {
-        id: "",
-        name: "",
-        averageRating: 0,
-        difficulty: 5.0,
-        type: "",
+        name: "To Heck With Pain",
+        averageRating: 3,
+        difficulty: "5.9 +",
+        type: "Sport",
     },
     {
-        id: "",
-        name: "",
-        averageRating: 0,
-        difficulty: 5.0,
-        type: "",
+        name: "Training Day",
+        averageRating: 3,
+        difficulty: 5.9,
+        type: "Sport, TR",
     },
     {
-        id: "",
-        name: "",
-        averageRating: 0,
-        difficulty: 5.0,
-        type: "",
-    },
-    {
-        id: "",
-        name: "",
-        averageRating: 0,
-        difficulty: 5.0,
-        type: "",
-    },
-    {
-        id: "",
-        name: "",
-        averageRating: 0,
-        difficulty: 5.0,
-        type: "",
-    },
-    {
-        id: "",
-        name: "",
-        averageRating: 0,
-        difficulty: 5.0,
-        type: "",
-    },
-    {
-        id: "",
-        name: "",
-        averageRating: 0,
-        difficulty: 5.0,
-        type: "",
-    },
-    {
-        id: "",
-        name: "",
-        averageRating: 0,
-        difficulty: 5.0,
-        type: "",
-    },
-    {
-        id: "",
-        name: "",
-        averageRating: 0,
-        difficulty: 5.0,
-        type: "",
+        name: "Travers Pastrami",
+        averageRating: 2,
+        difficulty: "5.10a",
+        type: "Sport",
     },
 ];
+
+const routesSection = document.querySelector(".routes");
+
+routes.forEach((route) => {
+    const section = document.createElement("section");
+    section.classList.add("card");
+    
+    section.innerHTML = `
+        <h3>${routes.name}</h3>
+        <p>${routes.type}</p>
+        <p>${routes.difficulty}</p>
+        <p>${routes.averageRating}</p>
+    `;
+    
+    routesSection.appendChild(section)
+});
+
+const cssSafeClass = routes.name
+    .toLowerCase()
+	.replace(/[.]/g, '')
+	.replace(/[ú]/g, 'u')
+	.replace(/[^a-z0-9]/g, '-')
+    .replace(/-+/g, '-');
+
+section.classList.add(cssSafeClass)
